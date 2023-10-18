08d4c3711824329d740976393bf16af28c042863
nensyah123 <nensyahninis@gmail.com>
2023-10-18 12:19:21 +0700

    Update SplashscreenActivity.java

--- app/src/main/java/com/olivia/laundry/SplashscreenActivity.java
+++ app/src/main/java/com/olivia/laundry/SplashscreenActivity.java
@@ -1,12 +1,26 @@
 package com.olivia.laundry;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
 public class SplashscreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splashscreen);
+        final Handler handler = new Handler(Looper.getMainLooper());
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashscreenActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        },1000);
+
     }
 }
