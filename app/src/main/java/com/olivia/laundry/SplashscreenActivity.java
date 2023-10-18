966bc6d864074f0d303c3accc29b2d8693c288ed
nensyah123 <nensyahninis@gmail.com>
2023-10-18 10:36:26 +0700

    Update SplashscreenActivity.java

--- app/src/main/java/com/olivia/laundry/SplashscreenActivity.java
+++ app/src/main/java/com/olivia/laundry/SplashscreenActivity.java
@@ -1,12 +1,24 @@
 package com.olivia.laundry;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-public class SplashscreenActivity extends AppCompatActivity {
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashscreenActivity extends AppCompatActivity {
+    Window window;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        window = getWindow();
         setContentView(R.layout.activity_splashscreen);
+
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
     }
 }
