30ccd48a9e0892c0bb9d6e41f5bcadd9ce8dc280
nensyah123 <nensyahninis@gmail.com>
2023-10-18 12:59:28 +0700

    Integrating Splashscreen to LoginActivity
    Co-Authored-By: RasyidahAisy <125231414+RasyidahAisy@users.noreply.github.com>
    Co-Authored-By: Irfan Marzenda <79095691+Marzenda@users.noreply.github.com>
    Co-Authored-By: SiTelur <70305841+SiTelur@users.noreply.github.com>

--- app/src/main/java/com/olivia/laundry/SplashscreenActivity.java
+++ app/src/main/java/com/olivia/laundry/SplashscreenActivity.java
@@ -6,12 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.Window;
 
 public class SplashscreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+
         final Handler handler = new Handler(Looper.getMainLooper());
 
         handler.postDelayed(new Runnable() {
@@ -19,10 +23,11 @@ public class SplashscreenActivity extends AppCompatActivity {
             public void run() {
                 Intent intent = new Intent(SplashscreenActivity.this, LoginActivity.class);
                 startActivity(intent);
+                finish();
             }
         },1000);
 
-        finish();
+
 
     }
 }
